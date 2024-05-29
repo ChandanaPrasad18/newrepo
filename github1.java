0fed5ab0577f9405b3074546bbd34ecc58f2dbfa
ChandanaPrasad18 <131746443+ChandanaPrasad18@users.noreply.github.com>
2024-05-29 15:49:31 +0530

    Update github1.java

--- github1.java
+++ github1.java
@@ -1,6 +1,6 @@
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello, GITHUB!");
+        System.out.println("Hello, GITHUB!, i have changed you to check pull");
     }
 }
