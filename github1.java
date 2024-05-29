946c458923e9a9b363322ec98169965557321803
ChandanaPrasad18 <131746443+ChandanaPrasad18@users.noreply.github.com>
2024-05-29 16:01:54 +0530

    Update github1.java

--- github1.java
+++ github1.java
@@ -1,6 +1,6 @@
 
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello, GITHUB!, modification of code is done");
+        System.out.println("Hello, GITHUB!");
     }
 }
